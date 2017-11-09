d48a51bd4caf53d1c3c8d72f687adcbf4ff72c23
deekshit99 <32210541+deekshit99@users.noreply.github.com>
2017-11-09 12:34:04 -0600

    faculty advisor

---
+++ CPSWebApplication/Views/FacultyAdvisorController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CPSWebApplication.Models.DB;
+
+namespace CPSWebApplication.Controllers
+{
+    public class FacultyAdvisorController : Controller
+    {
+        private CPSCreationEntities db = new CPSCreationEntities();
+
+        // GET: FacultyAdvisor
+        public ActionResult Index()
+        {
+            return View(db.StudentDetails.ToList());
+        }
+
+        // GET: FacultyAdvisor/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetail studentDetail = db.StudentDetails.Find(id);
+            if (studentDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetail);
+        }
+
+        // GET: FacultyAdvisor/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FacultyAdvisor/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "firstName,lastName,studentID,UHCLEmail,admittedSemester,currentSemester,majorName,CGPA,enrolledCoursesAndSemesters,completedCoursesAndGrades,ProgramCompletionType,AssignedFoundation")] StudentDetail studentDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StudentDetails.Add(studentDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentDetail);
+        }
+
+        // GET: FacultyAdvisor/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetail studentDetail = db.StudentDetails.Find(id);
+            if (studentDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetail);
+        }
+
+        // POST: FacultyAdvisor/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "firstName,lastName,studentID,UHCLEmail,admittedSemester,currentSemester,majorName,CGPA,enrolledCoursesAndSemesters,completedCoursesAndGrades,ProgramCompletionType,AssignedFoundation")] StudentDetail studentDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studentDetail);
+        }
+
+        // GET: FacultyAdvisor/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetail studentDetail = db.StudentDetails.Find(id);
+            if (studentDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetail);
+        }
+
+        // POST: FacultyAdvisor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            StudentDetail studentDetail = db.StudentDetails.Find(id);
+            db.StudentDetails.Remove(studentDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
